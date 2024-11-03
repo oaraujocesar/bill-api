4666ffe59abd8cf897cc81ce537e1e043f9a76ed
Caio Fernando <caiofsr@proton.me>
2024-11-03 17:55:19 -0300

    fix: development login

--- drizzle/0000_glamorous_stone_men.sql
+++ drizzle/0000_glamorous_stone_men.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS "account" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"serial" varchar(26) NOT NULL,
-	"name" varchar NOT NULL,
-	"balance" numeric(10, 2) DEFAULT 0.00 NOT NULL,
-	"user_id" uuid NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL,
-	"deleted_at" timestamp
+    "id" serial PRIMARY KEY NOT NULL,
+    "serial" varchar(26) NOT NULL,
+    "name" varchar NOT NULL,
+    "balance" numeric(10, 2) DEFAULT 0.00 NOT NULL,
+    "user_id" uuid NOT NULL,
+    "created_at" timestamp DEFAULT now() NOT NULL,
+    "updated_at" timestamp DEFAULT now() NOT NULL,
+    "deleted_at" timestamp
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "auth"."users" (
@@ -18,14 +18,14 @@ CREATE TABLE IF NOT EXISTS "auth"."users" (
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "user_profile" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"serial" varchar(26) NOT NULL,
-	"name" varchar NOT NULL,
-	"surname" varchar NOT NULL,
-	"birth_date" date NOT NULL,
-	"user_id" uuid NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL
+    "id" serial PRIMARY KEY NOT NULL,
+    "serial" varchar(26) NOT NULL,
+    "name" varchar NOT NULL,
+    "surname" varchar NOT NULL,
+    "birth_date" date NOT NULL,
+    "user_id" uuid NOT NULL,
+    "created_at" timestamp DEFAULT now() NOT NULL,
+    "updated_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
 DO $$ BEGIN
@@ -40,7 +40,24 @@ EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
-CREATE UNIQUE INDEX IF NOT EXISTS "account_serial_index" ON "account" USING btree ("serial");--> statement-breakpoint
-CREATE INDEX IF NOT EXISTS "account_user_id_index" ON "account" USING btree ("user_id");--> statement-breakpoint
-CREATE UNIQUE INDEX IF NOT EXISTS "profile_serial_index" ON "user_profile" USING btree ("serial");--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS "account_serial_index" ON "account" USING btree ("serial");
+--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "account_user_id_index" ON "account" USING btree ("user_id");
+--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS "profile_serial_index" ON "user_profile" USING btree ("serial");
+--> statement-breakpoint
 CREATE UNIQUE INDEX IF NOT EXISTS "profile_user_id_index" ON "user_profile" USING btree ("user_id");
+
+INSERT INTO
+    auth.users (
+        id,
+        email,
+        email_confirmed_at,
+        is_super_admin
+    )
+VALUES (
+        '6c6e1ced-a2cc-4dff-a15f-4d8926642b81',
+        'test_user@bill.com.br',
+        '2023-04-30',
+        true
+    );
