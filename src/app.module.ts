9cf13f61cf2e39a3e6381058bba04d7023c929ac
Caio Fernando <caiofsr@proton.me>
2024-12-18 16:50:40 -0300

    fix: change http folder to infra folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { HttpModule } from './http/http.module'
 import { DatabaseModule } from './infra/database/database.module'
+import { HttpModule } from './infra/http/http.module'
 import { SupabaseService } from './shared/services/supabase.service'
 
 @Module({
