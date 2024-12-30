9e0e69a60afe7e4de0583e4dde4947e53a237d02
Caio Fernando <caiofsr@proton.me>
2024-12-30 08:46:38 -0300

    feat: add cards table and update drizzle configuration for snake_case casing

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
 	schema: './src/infra/database/drizzle/schema/index.ts',
 	schemaFilter: ['public'],
 	dialect: 'postgresql',
+	casing: 'snake_case',
 	dbCredentials: {
 		url: process.env.DATABASE_URL ?? '',
 	},
