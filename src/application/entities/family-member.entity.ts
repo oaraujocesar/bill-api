fe840f844bf4cb4f601dca9cca9ab88f5ce81e22
César O. Araújo <cesar@mycroft.co>
2025-01-07 21:02:04 -0300

    BILL-013: create family owner when creating family

--- src/application/entities/family-member.entity.ts
+++ src/application/entities/family-member.entity.ts
@@ -1,38 +1,43 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { BaseEntity } from './helpers/base.entity'
 import { ULID } from 'ulidx'
+import { BaseEntity } from './helpers/base.entity'
 
 type FamilyMemberProps = {
-  id?: number
-  serial?: ULID
-  familyId: number
-  userId: string
-  createdAt?: Date
-  updatedAt?: Date
-  deletedAt?: Date
+	id?: number
+	serial?: ULID
+	familyId: number
+	isOwner?: boolean
+	userId: string
+	createdAt?: Date
+	updatedAt?: Date
+	deletedAt?: Date
 }
 
 export class FamilyMember extends BaseEntity {
-  @ApiProperty()
-  userId: string
+	@ApiProperty()
+	userId: string
 
-  @ApiProperty()
-  familyId: number
+	@ApiProperty()
+	familyId: number
 
-  constructor(props: FamilyMemberProps) {
-    super({
-      id: props.id,
-      serial: props.serial,
-      createdAt: props.createdAt,
-      updatedAt: props.updatedAt,
-      deletedAt: props.deletedAt,
-    })
+	@ApiProperty()
+	isOwner: boolean
 
-    this.familyId = props.familyId
-    this.userId = props.userId
-  }
+	constructor(props: FamilyMemberProps) {
+		super({
+			id: props.id,
+			serial: props.serial,
+			createdAt: props.createdAt,
+			updatedAt: props.updatedAt,
+			deletedAt: props.deletedAt,
+		})
 
-  static create(props: FamilyMemberProps) {
-    return new FamilyMember(props)
-  }
+		this.familyId = props.familyId
+		this.userId = props.userId
+		this.isOwner = props.isOwner
+	}
+
+	static create(props: FamilyMemberProps) {
+		return new FamilyMember(props)
+	}
 }
--- src/application/use-cases/family/create.ts
+++ src/application/use-cases/family/create.ts
@@ -1,5 +1,7 @@
 import { HttpStatus, Inject, Injectable, Logger } from '@nestjs/common'
+import { FamilyMember } from 'src/application/entities/family-member.entity'
 import { Family } from 'src/application/entities/family.entity'
+import { FamilyMemberRepository } from 'src/application/repositories/family-member.repository'
 import { FamilyRepository } from 'src/application/repositories/family.repository'
 import { FAMILY_REPOSITORY } from 'src/shared/tokens'
 import { ResponseBody, buildResponse } from 'src/shared/utils/build-response'
@@ -12,7 +14,10 @@ type CreateFamilyRequest = {
 export class CreateFamilyUseCase {
 	private readonly logger = new Logger(CreateFamilyUseCase.name)
 
-	constructor(@Inject(FAMILY_REPOSITORY) private readonly familyRepository: FamilyRepository) { }
+	constructor(
+		@Inject(FAMILY_REPOSITORY) private readonly familyRepository: FamilyRepository,
+		private readonly familyMemberRepository: FamilyMemberRepository,
+	) {}
 
 	async execute({ name }: CreateFamilyRequest, userId: string): Promise<ResponseBody<Family>> {
 		this.logger.debug('execution started')
@@ -23,7 +28,14 @@ export class CreateFamilyUseCase {
 
 		family.id = undefined
 
-		this.logger.debug(`family created ${JSON.stringify(family)}`)
+		this.logger.debug(`family created ${JSON.stringify(family)}.`)
+
+		this.logger.debug('creating family member (owner)...')
+
+		const familyMember = FamilyMember.create({ familyId: family.id, userId, isOwner: true })
+		await this.familyMemberRepository.save(familyMember)
+
+		this.logger.debug('family member (owner) created.')
 
 		return buildResponse({
 			data: family,
