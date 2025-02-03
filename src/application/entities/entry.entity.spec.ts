7121d256605e87c1dc0fa6932af768399a02c5fa
Caio Fernando <caiofsr@proton.me>
2025-02-02 21:11:31 -0300

    test: enhance Entry entity tests with timestamp validation and serial generation

--- src/application/entities/entry.entity.spec.ts
+++ src/application/entities/entry.entity.spec.ts
@@ -1,4 +1,6 @@
 import { faker } from '@faker-js/faker'
+import { DateTime } from 'luxon'
+import { isValid } from 'ulidx'
 import { Entry, EntryProps, EntryType } from './entry.entity'
 
 jest.mock('@nestjs/common/services/logger.service')
@@ -6,6 +8,12 @@ jest.mock('@nestjs/common/services/logger.service')
 describe('EntryEntity', () => {
 	it('should create entry', () => {
 		const input: EntryProps = {
+			id: faker.number.int(),
+			createdAt: faker.date.recent(),
+			updatedAt: faker.date.recent(),
+			paidAt: faker.date.recent(),
+			payday: faker.date.recent(),
+			serial: faker.string.ulid(),
 			accountId: faker.number.int(),
 			amount: faker.number.float(),
 			categoryId: faker.number.int(),
@@ -18,6 +26,12 @@ describe('EntryEntity', () => {
 		const entry = Entry.create(input)
 		expect(entry).toEqual(
 			expect.objectContaining({
+				id: input.id,
+				createdAt: expect.any(DateTime),
+				updatedAt: expect.any(DateTime),
+				paidAt: expect.any(DateTime),
+				payday: expect.any(DateTime),
+				serial: input.serial,
 				accountId: input.accountId,
 				amount: input.amount,
 				categoryId: input.categoryId,
@@ -27,5 +41,32 @@ describe('EntryEntity', () => {
 				type: input.type,
 			}),
 		)
+	})
+
+	it('should create timestamps', () => {
+		const input: EntryProps = {
+			id: faker.number.int(),
+			paidAt: faker.date.recent(),
+			payday: faker.date.recent(),
+			accountId: faker.number.int(),
+			amount: faker.number.float(),
+			categoryId: faker.number.int(),
+			description: faker.lorem.sentence(),
+			installments: 0,
+			title: faker.lorem.words(2),
+			type: EntryType.EXPENSE,
+		}
+
+		const entry = Entry.create(input)
+
+		expect(entry).toMatchObject({
+			...input,
+			serial: expect.any(String),
+			createdAt: expect.any(DateTime),
+			updatedAt: expect.any(DateTime),
+			paidAt: expect.any(DateTime),
+			payday: expect.any(DateTime),
+		})
+		expect(isValid(entry.serial)).toBe(true)
 	})
 })
